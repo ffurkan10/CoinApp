52d44ed7f5bb2defde51a7795d6e73c1f6f1100e
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-03-24 15:47:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
-      <ToastContainer position="bottom-right" />
+      <ToastContainer position="top-right" />
       <Router>
         <div className="app">
           <header>
